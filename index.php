82bf000071495ae4f3dc77a2b981cb94efc77008
paoloflore99 <paoloflore937@gmail.com>
2023-09-20 22:13:21 +0200

    creazione file

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__.'/class/Cane.php';
+require_once __DIR__.'/class/Categoria.php';
+require_once __DIR__.'/class/Cibo.php';
+require_once __DIR__.'/class/Cuccia.php';
+require_once __DIR__.'/class/Gatto.php';
+require_once __DIR__.'/class/Giochi.php';
+require_once __DIR__.'/class/Prodotti.php';
+
+
+
+
+?>
+
+<head>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="https://boolean.careers/favicon/favicon.ico">
+        <link rel="stylesheet" href="./css/style.css"><!--css-->
+        <title>PHP-OOP</title>
+    </head>
+<body>
+    
+
+
+    <script src="./js/main.js"></script>
+</body>
+</html>
